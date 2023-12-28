ab0cf02fe54e09c38af008744186835608451e07
dl <dinglei@jianli.com>
2023-12-28 18:00:42 +0800

    commit

---
+++ content/web2/go.mod
@@ -0,0 +1,9 @@
+module web2
+
+go 1.21.4
+
+require (
+	gee2 v0.0.0
+)
+
+replace gee2 => ./gee2
--- content/web2/main.go
+++ content/web2/main.go
@@ -8,24 +8,21 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"gee2"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
+	r := gee2.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
 
-// handler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
 
-// handler echoes r.URL.Header
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
+	r.Run(":9999")
 }
